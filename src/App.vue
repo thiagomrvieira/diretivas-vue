84e93b50e8b2e2d7df26e8adeddac747347491dc
Thiago Maciel <thiagomrvieira@gmail.com>
2020-02-01 20:14:15 +0000

    usando multiplos modificadores

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,11 @@
 
 		<!-- Diretiva registrada localmente -->
 		<p v-destaque-local:fundo.atrasar ="cor">Usando diretiva personalizada (local)</p>
+		<hr>
+
+		<!-- Usando múltiplos modificadores -->
+		<p v-destaque-local:fundo.atrasar.alternar ="cor">Usando múltiplos modificadores</p>
+
 
 	</div>
 </template>
@@ -31,16 +36,33 @@ export default {
 	directives: {
 		'destaque-local' : {
 			bind(el, binding, vnode){
-		
+				
+				const aplicarCor = cor => {
+					if (binding.arg === "fundo") {
+						el.style.backgroundColor = cor
+					} else {
+						el.style.color = cor
+					}
+				}
+
+
 				let atraso = 0
 				if (binding.modifiers['atrasar']) atraso = 3000
 				
 				setTimeout(() => {
-					if (binding.arg === "fundo") {
-						el.style.backgroundColor = binding.value
-					} else {
-						el.style.color = binding.value
+					if (binding.modifiers['alternar']){
+						 setInterval(() =>{
+							 corAtual = corAtual === cor1 ? cor2 : cor1
+							 aplicarCor(corAtual)
+						 }, 1000)
+					}else{
+						aplicarCor(binding.value)
 					}
+
+					const cor1 = binding.value
+					const cor2 = 'purple'
+					let corAtual = cor1
+
 				}, atraso)
 			}
 		}
